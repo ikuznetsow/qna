af31c086b5f6d0dfc6f835fa4c8edd0b1c4f71a3
ilya <ilya@awb.io>
2015-11-27 23:30:27 +0100

    questions controller basics

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,13 @@
+class QuestionsController < ApplicationController
+	def index
+		@questions = Question.all
+	end
+
+	def show
+		@question = Question.find(params[:id])
+	end
+
+	def new
+		@question = Question.new
+	end
+end
---
+++ spec/controllers/questions_controller_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe QuestionsController, type: :controller do
+	describe 'GET #index'	do
+		let(:questions) { create_list(:question, 2) } 
+
+		before { get :index } 
+		
+		it 'populates array of all questions' do
+			expect(assigns(:questions)).to match_array(questions)
+		end
+		
+		it 'renders index view' do
+			expect(response).to render_template :index
+		end
+	end
+
+	describe 'GET #show' do
+		let(:question) { create(:question) } 
+
+		before { get :show, id: question }
+
+		it 'assigns requested questions to @question' do
+			expect(assigns(:question)).to eq question	
+		end
+		
+		it 'renders show view' do
+			expect(response).to render_template :show
+		end
+	end
+
+	describe 'GET #new' do
+		before { get :new }
+
+		it 'assigns a new question to @question' do
+			expect(assigns(:question)).to be_a_new(Question)
+		end
+		it 'renders new view' do
+			expect(response).to render_template :new
+		end
+	end
+end
