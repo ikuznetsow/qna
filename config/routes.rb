73c2a69b03c1ad0602f8ec8673df56f1ea01450d
ilya <ilya@awb.io>
2016-01-17 01:53:04 +0100

    + add_files_to_question spec

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,15 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
+  
   root to: 'questions#index'
 
   resources :questions do
     resources :answers
     patch 'answers/:id/set_best(.:format)' => 'answers#set_best', as: 'answer_set_best'
-      # patch :set_best
-    
   end
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
+  
   # Example resource route with options:
   #   resources :products do
   #     member do
